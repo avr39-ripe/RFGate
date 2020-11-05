0c3aeb2499ed79b122e0caae3532002ac0e386a7
Alexander V. Ribchansky <avr39ripe@gmail.com>
2020-11-05 17:32:30 +0200

    Initial commit of RFGate application
    Initial commit of RFGate application. Just basik RF things implemented.

---
+++ component.mk
@@ -0,0 +1,26 @@
+ARDUINO_LIBRARIES :=  ArduinoJson5 RCSwitch
+
+SPI_SIZE		?= 4M
+
+RBOOT_ENABLED		?= 1
+RBOOT_BIG_FLASH		?= 1
+RBOOT_TWO_ROMS		?= 0
+RBOOT_SPIFFS_0		?= 0x100000
+RBOOT_SPIFFS_1		?= 0x300000
+RBOOT_RTC_ENABLED	= 1
+
+DISABLE_SPIFFS		?= 0
+SPIFF_SIZE		?= 524288
+
+ENABLE_CMD_EXECUTOR	= 0
+
+COMPONENT_SRCDIRS = app lib/application
+COMPONENT_INCDIRS = include lib/application lib/wsbinconst
+
+WEBPACK_OUT = files/index.*.js*
+
+.PHONY: webpacker
+webpacker: 
+	$(vecho) "Bundling js with Webpack"
+	$(Q) rm -rf $(WEBPACK_OUT)
+	$(Q) webpack
